e67090ccee22a52e5059ddd7a45d68a47fd81686
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-11 12:26:09 +0600

    login and register form done

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -1,10 +1,77 @@
+import { Link } from "react-router-dom";
 
 function Register() {
-    return (
-        <div>
-            
+    
+    const hundleRegister = (event) =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        console.log(name, email, password);
+
+    }
+
+  return (
+    <>
+      <div className="hero mt-32">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-3xl font-bold">Register now!</h1>
+          </div>
+          <div className="card bg-base-100 w-[800px] max-w-lg shrink-0 shadow-2xl">
+            <form onSubmit={hundleRegister} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+            </form>
+            <p className="text-center text-yellow-500 py-3">
+              If you have an Account Please{" "}
+              <Link to="/login">
+                <u className="text-green-500">Login</u>
+              </Link>
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
